2a3d394507fe202d00618b3dfd848ee8784c91fa
Muhammad Saad Ibrahim <isaadibrahim2005@gmail.com>
2024-01-16 11:09:44 +0500

    test port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const mysql = require("mysql2");
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
