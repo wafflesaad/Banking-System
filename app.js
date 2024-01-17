33b3b4c5f6e434c19635aa8fd8605bc4717e2620
Muhammad Saad Ibrahim <isaadibrahim2005@gmail.com>
2024-01-17 22:10:58 +0500

    Added transaction functionality

--- app.js
+++ app.js
@@ -30,9 +30,18 @@ const cutomerSchema = new mongoose.Schema({
     name: String,
     email: String,
     balance: Number,
-    address: String
+    address: String,
+    accountno: String
 });
 
+ const transactionSchema = new mongoose.Schema({
+    from: String,
+    to: String,
+    amount: Number,
+    date: Date
+  });
+
+const transaction = mongoose.model("transaction", transactionSchema);
 const customer = mongoose.model("customer", cutomerSchema);
 
 
@@ -50,6 +59,112 @@ app.post("/add", (req,res)=>{
   
   newcust.save();
   res.sendStatus(200);
+});
+
+app.post("/update", (req,res)=>{
+  let body = req.body;
+  console.log(body);
+
+  customer.updateOne({name: body.name}, {$set:{accountno: body.accountno}}).then((result)=>{
+    console.log("Updated");
+    console.log(result);
+  }).catch((err)=>{
+    console.log("Error updating");
+    console.log(err);
+  });
+
+  res.sendStatus(200);
+});
+
+//expected transaction body: from, to, amount
+
+app.post("/transact", async (req,res)=>{
+  let body = req.body;
+
+  async function findFrom(){
+    console.log("findfrom started");
+    await customer.findOne({accountno: body.from}).then((result)=>{
+      if(!result){
+        console.log("No from entry found");
+        //todo deal with no entry found case
+      }
+      console.log(result);
+      
+      if(result.balance < body.amount){
+        console.log("Insufficient balance");
+        //todo deal with insufficient balance case
+      }
+
+    }).catch((err)=>{
+      console.log(err);
+      res.sendStatus(204);
+    });
+    console.log("findfrom ended");
+  }
+
+  async function findTo(){
+    console.log("findto started");
+    await customer.findOne({accountno: body.to}).then((result)=>{
+      if(!result){
+        console.log("No to entry found");
+        //todo deal with no entry found case
+      }
+      console.log(result);
+    }).catch((err)=>{
+      console.log(err);
+      res.sendStatus(204);
+    });
+    console.log("findto ended");
+  }
+
+
+  async function updateFrom(){
+    console.log("updatefrom started");
+    await customer.updateOne({accountno: body.from},{$inc:{balance: -body.amount}},).then((result)=>{
+      console.log("Updated from");
+      console.log(result);
+    }).catch((err)=>{
+      console.log("Error updating from");
+      console.log(err);
+    });
+    console.log("updatefrom ended");
+  }
+
+
+  async function updateTo(){
+    console.log("updateto started");
+    await customer.updateOne({accountno: body.to},{$inc:{balance: body.amount}},).then((result)=>{
+      console.log("Updated to");
+      console.log(result);
+    }).catch((err)=>{
+      console.log("Error updating to");
+      console.log(err);
+    });
+
+    console.log("updateto ended");
+  }
+
+  await findFrom();
+  await findTo();
+  await updateFrom();
+  await updateTo();
+
+  //adding entry to transaction history
+  let entry = new transaction({
+    from: body.from,
+    to: body.to,
+    amount: body.amount,
+    date: new Date()
+  });
+
+  entry.save().then((result)=>{
+    console.log(result);
+  }).catch((err)=>{
+    console.log(err);
+  });
+  
+  res.sendStatus(200);
+
 });
 
 app.get("/",(req,res)=>{
